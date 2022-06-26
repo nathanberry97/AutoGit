4e13ef4433a7fcdbde01adb662af35ffb47cd904
nathanberry97 <nathanberry97@live.com>
2022-06-26 21:33:11 +0100

    first

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-git add -A
-
-git commit -m $0
+while getopts ":c:" opt; do
+  case $opt in
+    c)
+      echo "-c was triggered, Parameter: $OPTARG"
+      git add -A
+      git commit -m $OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      exit 1
+      ;;
+  esac
+done
