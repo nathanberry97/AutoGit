510eb3c75b32ca41a61b3bfc7712f9158b2faa1b
nathanberry97 <nathanberry97@live.com>
2022-06-26 22:20:31 +0100

    ''
    'test
    '
    
    added help function
    
    updated to functions
    
    added git squash function
    
    added git squash function
    
    added git squash function

--- src/main.sh
+++ src/main.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-while getopts ":c:" opt; do
+git_commit() {
+  git add -A
+  git commit -m "$OPTARG"
+  exit 0
+}
+
+git_squash() {
+  git rebase -i HEAD~$OPTARG
+  exit 0
+}
+
+auto_help() {
+  echo -e "AutoGit - git made easy\n"
+  echo -e "Usage: ag [argument] string\n"
+  echo "Arguments:"
+  echo "  -c        add and commit changes"
+  echo "  -h        help menu"
+  exit 0
+}
+
+while getopts ":c:s:h?" opt; do
   case $opt in
     c)
-      echo "-c was triggered, Parameter: $OPTARG"
-      git add -A
-      git commit -m "'$OPTARG'"
-      ;;
+      git_commit;;
+    s)
+      git_squash ;;
+    h)
+      auto_help ;;
     \?)
       echo "Invalid option: -$OPTARG"
       exit 1
