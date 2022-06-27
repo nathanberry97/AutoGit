a5d98c1becb13b9fee51bd3d2c354c22b733a06d
nathanberry97 <nathanberry97@live.com>
2022-06-27 21:27:38 +0100

    added methods to now add branches and initialise git repos

--- src/main.sh
+++ src/main.sh
@@ -6,6 +6,22 @@ git_commit() {
   exit 0
 }
 
+git_branch() {
+  git branch $OPTARG
+  git checkout $OPTARG
+  exit 0
+}
+
+git_new_repo() {
+  touch README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git remote add origin https://github.com/nathanberry97/$OPTARG.git
+  git push -u origin master
+  exit 0
+}
+
 git_squash() {
   git rebase -i HEAD~$OPTARG
   exit 0
@@ -13,9 +29,12 @@ git_squash() {
 
 auto_help() {
   echo -e "AutoGit - git made easy\n"
-  echo -e "Usage: ag [argument] string\n"
+  echo -e "Usage: ag [argument] string"
+  echo -e "   or: ag [argument]\n"
   echo "Arguments:"
+  echo "  -r        create new repo and push to github"
   echo "  -c        add and commit changes"
+  echo "  -b        create new branch and checkout into it"
   echo "  -s        squash commits"
   echo "  -h        help menu"
   exit 0
@@ -26,15 +45,19 @@ invalid_option() {
   exit 1
 }
 
-argement_needed() {
+argument_needed() {
   echo "Option -$OPTARG requires an argument."
   exit 1
 }
 
-while getopts ":c:s:h?" opt; do
+while getopts ":r:c:b:s:h?" opt; do
   case $opt in
+    r)
+      git_new_repo;;
     c)
       git_commit;;
+    b)
+      git_branch;;
     s)
       git_squash;;
     h)
@@ -42,6 +65,6 @@ while getopts ":c:s:h?" opt; do
     \?)
       invalid_option;;
     :)
-      argement_needed;;
+      argument_needed;;
   esac
 done
